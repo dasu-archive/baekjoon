9383d359eb9b1e1a265ed9e1b65127bd56e04c29
dasu <dasu.archive@gmail.com>
2025-12-17 19:21:31 +0900

    34789 이건 답이 없다. 아무리 생각해도 모르겠다

---
+++ in_progress/34898.java
@@ -0,0 +1,49 @@
+// https://www.acmicpc.net/problem/34898
+
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+import java.util.Comparator;
+import java.util.StringTokenizer;
+
+class Main_34898{
+
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        int N = Integer.parseInt(st.nextToken());
+        int M = Integer.parseInt(st.nextToken());
+
+        int[] a = new int[N];
+        int[] b = new int[N];
+        int[] c = new int[N];
+
+        for (int i = 0; i < N; i++) {
+            st = new StringTokenizer(br.readLine());
+            a[i] = Integer.parseInt(st.nextToken());
+            b[i] = Integer.parseInt(st.nextToken());
+            c[i] = Integer.parseInt(st.nextToken());
+        }
+
+        int[] dp = new int[M + 1];
+
+
+    }
+
+}
+
+
+
+
+
+
+
+
+
+
+
+
